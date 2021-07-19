e5383e22ca95a4a391e8d0f813d36b240bcf6eb8
AnushaMotamarri <motamarrianusha01@gmail.com>
2021-07-19 16:23:51 +0530

    diff b/w splice and slice

---
+++ q7.js
@@ -0,0 +1,16 @@
+// what are the differences between slice and splice?
+
+/*
+
+Slice	
+Doesn't modify the original array(immutable)	
+Returns the subset of original array	
+Used to pick the elements from array	
+*/
+
+/*
+Splice
+Modifies the original array(mutable)
+Returns the deleted elements as array
+Used to insert or delete elements to/from array
+*/
