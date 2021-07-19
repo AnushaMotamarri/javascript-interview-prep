443067b80e9e13256faf0fd5ebe343134d4eea30
AnushaMotamarri <motamarrianusha01@gmail.com>
2021-07-19 21:34:09 +0530

    modules

---
+++ q23.js
@@ -0,0 +1,9 @@
+//What is the benefit of using modules
+
+/*
+There are a lot of benefits to using modules in favour of a sprawling. Some of the benefits are,
+
+Maintainability
+Reusability
+Namespacing
+*/
