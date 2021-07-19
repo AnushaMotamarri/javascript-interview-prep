20a2caa9bf6b5449ebb9318e19f7d404cc1afba5
AnushaMotamarri <motamarrianusha01@gmail.com>
2021-07-19 21:32:29 +0530

    diff b/w let and var

---
+++ q18.js
@@ -0,0 +1,13 @@
+//What is the difference between let and var
+/*
+var	
+It is been available from the beginning of JavaScript	
+It has function scope	
+Variables will be hoisted	
+
+let
+Introduced as part of ES6
+	It has block scope
+    Hoisted but not initialized
+
+    */
