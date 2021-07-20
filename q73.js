af8f89b14b01cd738a662bdc779ee63d12a49ff7
AnushaMotamarri <motamarrianusha01@gmail.com>
2021-07-20 14:59:38 +0530

    null

---
+++ q73.js
@@ -0,0 +1,8 @@
+//What is null value
+
+/*
+The value null represents the intentional absence of any object value. typeof null =object  this is a bug in JS
+*/
+
+var user = null;
+console.log(typeof(user))
